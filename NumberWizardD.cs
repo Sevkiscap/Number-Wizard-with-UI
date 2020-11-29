bd0eba32af6cf242fca1ba5d67c3ed5d3822f962
Sevkiscap <63850097+Sevkiscap@users.noreply.github.com>
2020-11-29 10:18:13 -0700

    first commit

---
+++ NumberWizardD.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizardD : MonoBehaviour
+{
+   [SerializeField] int max;
+   [SerializeField] int min;
+   [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    Guess();
+
+
+    }
+    public void Guess ()
+    {
+      
+      guess = Random.Range(min, max+1);
+      guessText.text = guess.ToString();
+
+    }
+
+
+    public void PressHigher()
+    {
+        min = guess+1;
+        Guess();
+    }
+    public void PressLower()
+    {
+        max = guess-1;
+        Guess();
+    }
+    
+}
---
+++ Restart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+
+    public void LoadScene()
+    {
+
+        int CurrenScenetIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(0);
+    }
+
+}
